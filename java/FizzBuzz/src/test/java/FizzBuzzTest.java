56368c7ade89b73d0da700a0ed28b61909c7b3f6
mca <mca@bec.dk>
2019-11-13 15:01:24 +0100

    added fizzbuzz kata

---
+++ java/FizzBuzz/src/test/java/FizzBuzzTest.java
@@ -0,0 +1,21 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    @Test
+    @Ignore
+    public void printFizz() {
+
+    }
+
+    @Test
+    @Ignore
+    public void printBuzz() {
+    }
+
+    @Test
+    @Ignore
+    public void printFizzBuzz() {
+
+    }
+}
